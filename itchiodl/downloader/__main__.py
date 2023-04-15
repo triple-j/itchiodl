8b96613393d8995fde3491d64b81354330785092
Jeremie J. Jarosh <jeremie@jarosh.org>
2023-04-15 11:03:44 -0500

    Update `--help` Description (cont.)

--- itchiodl/downloader/__main__.py
+++ itchiodl/downloader/__main__.py
@@ -6,7 +6,9 @@ import itchiodl
 
 
 def main():
-    parser = argparse.ArgumentParser(prog="itch-download", description="Download / archive your itch.io library.")
+    parser = argparse.ArgumentParser(
+        prog="itch-download", description="Download / archive your itch.io library."
+    )
 
     parser.add_argument(
         "-k", "--api-key", help="Use API key instead of username/password"
