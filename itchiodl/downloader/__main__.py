bebd8064c6b3b8c0904e26087d278390d58cf812
Jeremie J. Jarosh <jeremie@jarosh.org>
2023-04-15 13:22:16 -0500

    Keep consistent style when listing platform types

--- itchiodl/downloader/__main__.py
+++ itchiodl/downloader/__main__.py
@@ -31,7 +31,7 @@ def main():
         "--platform",
         help=(
             "Platform to download for (default: all), will accept values like 'windows', 'linux', "
-            "'osx' and android"
+            "'osx' and `android`"
         ),
     )
 
